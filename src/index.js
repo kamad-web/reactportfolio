ccc773b0a66f960813ef5db4f360c48f2ec24c9d
kamad-web <kamad.sharma.964@gmail.com>
2021-01-16 17:16:06 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+// import Data from "./Data";
+
+ReactDOM.render(
+  <>
+  <h1>Top 5 web series</h1> 
+  </>,
+  document.getElementById('root')
+);
+
